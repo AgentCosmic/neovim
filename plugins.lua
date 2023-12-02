eccfa3073f801fc9a5b83b5b282c7d91959f5026
Dalton Tan <daltonyi@hotmail.com>
2023-12-02 23:02:08 +0800

    lsp signs

--- plugins.lua
+++ plugins.lua
@@ -346,7 +346,7 @@ require('lazy').setup({
 				virtual_text = { prefix = '' },
 			})
 			-- diagnostic signs
-			local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+			local signs = { Error = '', Warn = '', Hint = '', Info = '' }
 			for type, icon in pairs(signs) do
 				local hl = 'DiagnosticSign' .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl })
