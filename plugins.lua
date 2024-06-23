5301f6e7655dce33704664cef529ba4f38e73f0b
Dalton Tan <daltonyi@hotmail.com>
2024-06-23 15:07:17 +0800

    add codeium

--- plugins.lua
+++ plugins.lua
@@ -296,6 +296,14 @@ require('lazy').setup({
 		end
 	},
 
+	{
+		'Exafunction/codeium.vim',
+		event = 'BufEnter',
+		config = function()
+			vim.keymap.set('i', '<c-b>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+		end
+	},
+
 
 	------------------------------------------------------------------------------
 	-- LSP
